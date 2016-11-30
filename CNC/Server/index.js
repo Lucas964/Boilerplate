fc97c331ddcc9abe8397a1f6fe49c8a44a176be6
Eugen <Eugen@141.19.156.75>
2016-11-30 15:26:56 +0100

    Was Gescheites

--- CNC/Server/index.js
+++ CNC/Server/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
+const fs = require('fs');
+
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+fs.writeFile('test.txt', 'Index.js: This will never work!', 'utf8');
