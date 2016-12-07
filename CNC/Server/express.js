060ba8c029e7464935093f2ee6c9e33a5cd10de4
Eugen <Eugen@141.19.158.159>
2016-12-07 14:34:06 +0100

    express.js test thingy

---
+++ CNC/Server/express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app     = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.get('/kek', (req, res) => {
+    res.send('Hello wor- kek!!1!!');
+});
+
+app.listen(3000, () => {
+    console.log('Example listening on http://localhost:3000');
+});
